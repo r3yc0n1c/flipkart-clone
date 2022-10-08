c13eb40f374566016b0a142ed340fa3a63998830
Sinjini Ghosh <sg18062003@gmail.com>
2022-10-08 12:56:09 +0530

    login feature added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const fs = require("fs")
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
@@ -12,7 +13,32 @@ app.get("/test",(req,res)=>{
 app.post("/signup",(req,res)=>{
     const {name,email,pswd}=req.body
     console.log(name,email,pswd)
-    res.status(200).send("SignUp successful")
+
+    const data=JSON.stringify({email:{"name":name,"password":pswd}})
+    fs.writeFile("db.json",data, "utf8", (err)=>{
+        if(err){
+            console.log(err)
+            res.status(500).send("SignUp error")
+        }
+        res.status(200).send("SignUp successful")
+    })
+})
+
+app.post("/login",(req,res)=>{
+    const {email,pswd}=req.body
+    console.log(email,pswd)
+    fs.readFile("db.json", "utf8", (err,jsondata)=>{
+        if(err){
+            console.log(err)
+            res.status(500).send("Login error")
+        }
+        const data=JSON.parse(jsondata)
+        console.log("data=",data)
+        if(pswd===data.email.password)
+            res.status(200).send("Login successful")
+        else
+            res.status(401).send("Login unsuccessful")
+    })
 })
 
 app.listen(PORT,()=>{
