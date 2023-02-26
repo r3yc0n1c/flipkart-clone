9d489817e3048e14061dea7b503894232143dfc2
r3yc0n1c <raja.majumdar.01@gmail.com>
2023-02-27 03:54:12 +0530

    feat: Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,34 @@
 import { Link } from "react-router-dom"
-import { Box, AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material"
+import { Box, AppBar, Toolbar, IconButton, Typography, TextField, Button } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchBar = ({ setSearchQuery }) => (
+  <form>
+    <TextField
+      id="search-bar"
+      className="text"
+      onInput={(e) => {
+        setSearchQuery(e.target.value);
+      }}
+      // label="Enter a city name"
+      variant="outlined"
+      placeholder="Search..."
+      size="small"
+      sx={{
+        // "& .MuiInputLabel-root": {bgcolor: "white", color: 'white'},//styles the label
+        bgcolor: "white",
+        "& .MuiOutlinedInput-root": {
+          "& > fieldset": { border: "none" },
+        },
+      }}
+
+
+    />
+    <IconButton type="submit" aria-label="search" sx={{bgcolor:"white", borderRadius: 0, "&:hover":{bgcolor: "white"} }}>
+      <SearchIcon style={{ fill: "#1976D2" }} />
+    </IconButton>
+  </form>
+);
 
 const Navbar = () => {
   // return (
@@ -13,12 +42,15 @@ const Navbar = () => {
 
   return (
     <Box sx={{ flexgrow: 1 }}>
-      <AppBar position="static" sx={{ textTransform: "capitalize" }}>
+      <AppBar position="static" sx={{ textTransform: "capitalize", height: 55 }}>
         <Toolbar>
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Flipkart
           </Typography>
+
+          <SearchBar />
+
 
           <Button color="inherit">User</Button>
           <Button color="inherit">Become a Seller</Button>
